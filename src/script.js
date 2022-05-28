0c26bd4200bc706ff359fd8158f0f399361f6c2e
Lakshan Bandara <lakshanwarunab@gmail.com>
2022-05-28 07:32:52 +0530

    initail code setup

--- src/script.js
+++ src/script.js
@@ -1,37 +1,30 @@
 import "./style.css";
 import * as THREE from "three";
 
-// Task: Add a red cube.
+// Canvas
+const canvas = document.querySelector(".webgl");
 
-// Step 01 - Add the scene.
+// Scene
 const scene = new THREE.Scene();
 
-// Step 02 - Add Geometry.
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// Step 03 - Add Meterial.
 const material = new THREE.MeshBasicMaterial({ color: 0xf24c4c });
-
-// Step 04 - Add Mesh.
 const mesh = new THREE.Mesh(geometry, material);
-
-// Step 05 - Add mesh to the scene.
 scene.add(mesh);
 
-// Step 06 - Define sizes.
+// Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// Step 07 - Add Camera.
+// Camera.
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// Step 08 - Move the camera back.
 camera.position.z = 3;
 scene.add(camera);
 
-// Step 09 - Add renderer.
-const canvas = document.querySelector(".webgl");
+// Renderer.
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
